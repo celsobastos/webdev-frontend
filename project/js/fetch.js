c10e20984d00e683b20e0f41084d6183c0fb3177
= <=>
2021-01-09 15:32:29 -0300

    new features

---
+++ project/js/fetch.js
@@ -0,0 +1,38 @@
+let btnSearch = document.getElementById('btn-search');
+btnSearch.addEventListener('click', function () {
+    executeSearch();  
+})
+
+let textSearchPress = document.getElementById('search');
+textSearchPress.addEventListener('keyup', function () {
+    executeSearch();    
+})
+
+function executeSearch() {
+    let textSearch = document.getElementById('search').value.trim();
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const divResultado  = document.getElementById('resultado');
+
+    search(textSearch, url, divResultado);
+}
+
+function search(name , url, div) {
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+        var error = false;
+        var test = data.map((data) => {
+            if(name == data.name ){
+                div.innerHTML = '<div>' +  data.name + ' - ' + data.email + '</div>';
+                error = true;
+            }
+        })
+        if(!error){
+            div.innerHTML = 'Ops! Nome não existe.';
+        }
+        return test;
+    })
+    .catch((error) => {
+        console.log("Ops! Algo deu errado")
+    })
+}
