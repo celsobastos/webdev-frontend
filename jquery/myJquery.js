63cd65cce092b49c0b6894150d061ff2467ed863
= <=>
2021-01-16 11:27:36 -0300

    new features

--- jquery/myJquery.js
+++ jquery/myJquery.js
@@ -17,19 +17,58 @@ $(function () {
     */
 
     //getter
-    let contentHeader = $('header').html();
-    let contentAttr = $('header').attr('class');
+    //let contentHeader = $('header').html();
+    //let contentAttr = $('header').attr('class');
+    //let contentInput = $('input').val();
+
+    //if(contentInput.length > 10){
+    //    $('input[type="text"]').css('border','2px solid red');
+    //}
 
 
     //setter
     //$('header').html('Meu test');
     //$('header').html(contentAttr);
-    $('header').html(function (i, content) {
-        return "olá " + content + " new element";
-    });
-    $('header').attr("class","error");
+    //$('header').html(function (i, content) {
+    //    return "olá " + content + " new element";
+    //});
+    //$('header').attr("class","error");
 
+    //APPEND
+    let contentHeader = $('header').html();
+    $('header').append("test 2")
+    $('header').prepend("test 1")
+    //$('header').after(contentHeader)
+    //$('header').before(contentHeader)
 
+    /*
+    $('img').click(function () {
+        let attrAlt = $(this).attr('alt');
+        $(this).width('20%')
+    })
+    */
+
+   /*
+   $('img').on('click mouseout', function () {
+
+        let attrAlt = $(this).attr('alt');
+        $(this).width('20%')
+
+   })*/
+
+   $('img').on({
+       mouseover:function () {
+           $('body').css('background-color', 'red');
+       }
+   },{
+       mouseout:function () {
+        $('body').css('background-color', 'blue');
+       } 
+   },{
+       click:function () {
+        $('body').css('background-color', 'green');
+       } 
+   })
 
 
 
